31adb537f34e1c2e47411793f538d1cedfec31bf
andy silva <andssilva.23@gmail.com>
2021-02-24 12:51:30 -0400

    JavaScript: primeiros passos - Parte III

--- exercises/bloco_4/dia_1/scripts.js
+++ exercises/bloco_4/dia_1/scripts.js
@@ -1,13 +1,43 @@
+/* Parte 2 - Conteúdo */
 let name = "Anderson Silva";
 let birthCity = "Manaus/AM";
 let birthYear = 1994;
 
-console.log(name, birthCity, birthYear);
+/* console.log(name, birthCity, birthYear); */
 
 birthYear = 2020;
 
-console.log(birthYear);
+/* console.log(birthYear); */
 
 birthCity = "Curitiba/PA";
 
-console.log(birthCity)
+/* console.log(birthCity); */
+
+/* Parte 3 - Conteúdo */
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+/* console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge); */
+
+patientId = "50"
+/* console.log(typeof patientId); */
+
+let base = 5;
+let altura = 8;
+
+let area = base * altura;
+
+console.log(area);
+
+let perimetro = (2 * base) + (2 * altura);
+
+console.log(perimetro);
