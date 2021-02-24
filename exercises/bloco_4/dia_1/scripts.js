2125cf2b0658df31be2aa983e35541059e1aec7c
andy silva <andssilva.23@gmail.com>
2021-02-24 12:40:58 -0400

    JavaScript: primeiros passos - Parte II

---
+++ exercises/bloco_4/dia_1/scripts.js
@@ -0,0 +1,13 @@
+let name = "Anderson Silva";
+let birthCity = "Manaus/AM";
+let birthYear = 1994;
+
+console.log(name, birthCity, birthYear);
+
+birthYear = 2020;
+
+console.log(birthYear);
+
+birthCity = "Curitiba/PA";
+
+console.log(birthCity)
