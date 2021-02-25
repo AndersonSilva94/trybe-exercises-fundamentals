2d963fbe5ea9f5fd58fdbed8928b8ae39ae7a50f
andy silva <andssilva.23@gmail.com>
2021-02-24 21:03:27 -0400

    questão 1

---
+++ exercises/bloco_4/dia_1/exercicio.js
@@ -0,0 +1,11 @@
+/* Questão 1 */
+let a = 8;
+let b = 5;
+
+let soma = a + b;
+let subtracao = a - b;
+let divisao = a / b;
+let multiplicacao = a * b;
+let modulo = a % b;
+
+console.log(soma, subtracao, divisao, multiplicacao, modulo)
