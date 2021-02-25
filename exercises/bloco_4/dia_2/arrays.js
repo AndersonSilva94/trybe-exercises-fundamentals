2f86dcc59a370a1c4b0bb69686300d0a1c46cf4a
andy silva <andssilva.23@gmail.com>
2021-02-25 14:25:32 -0400

    conteúdo 4.2: exercício de arrays - questão 2

--- exercises/bloco_4/dia_2/arrays.js
+++ exercises/bloco_4/dia_2/arrays.js
@@ -3,3 +3,9 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+
+/* Questão 2 */
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
