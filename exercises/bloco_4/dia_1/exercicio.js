94e7598e17dd2fa264824d2fe36415b5e879ca4e
andy silva <andssilva.23@gmail.com>
2021-02-24 22:12:18 -0400

    questão 4, questão 5

--- exercises/bloco_4/dia_1/exercicio.js
+++ exercises/bloco_4/dia_1/exercicio.js
@@ -14,21 +14,43 @@ let modulo = a % b;
 let num1 = 8;
 let num2 = 5;
 
-/* if(num1 > num2){
-  console.log(num1)
+if(num1 > num2){
+  console.log(num1);
 }else{
-  console.log(num2)
-} */
+  console.log(num2);
+}
 
 /* Questão 3 */
 let num3 = 8;
 let num4 = 5;
 let num5 = 29;
 
-if(num3 > num4 && num3 > num5){
-  console.log(num3)
+if (num3 > num4 && num3 > num5) {
+  console.log(num3);
 }else if(num4 > num3 && num4 > num5){
-  console.log(num4)
+  console.log(num4);
 }else{
-  console.log(num5)
+  console.log(num5);
+}
+
+/* Questão 4 */
+let positivoNegativo = 0;
+
+if(positivoNegativo > 0 ){
+  console.log("positivo");
+}else if(positivoNegativo < 0 ){
+  console.log("negativo");
+}else{
+  console.log("zero");
+}
+
+/* Questão 5 */
+let lado1 = 50;
+let lado2 = 30;
+let lado3 = 110;
+
+if(lado1 + lado2 + lado3 === 180){
+  console.log(true)
+}else{
+  console.log(false)
 }
