11fbf08eeb5bb4df864a4358c6177b0f0ef28f3c
andy silva <andssilva.23@gmail.com>
2021-02-26 03:55:56 -0400

    exercicios - questão 9, renomeando arquivos

--- exercises/bloco_4/dia_2/exercicios8-9.js
+++ exercises/bloco_4/dia_2/exercicios8-9.js
@@ -6,3 +6,8 @@ for(let index = 1; index <= 25; index += 1){
 }
 
 console.log(novoArray)
+
+//Questão 9
+for(let index = 0; index < novoArray.length; index += 1){
+  console.log(novoArray[index] / 2)
+}
