44b5364b1c6ef328959617af1e2081783b96c96f
andy silva <andssilva.23@gmail.com>
2021-03-08 11:49:05 -0400

    questão 1

--- exercises/bloco_5/dia_3/script.js
+++ exercises/bloco_5/dia_3/script.js
@@ -14,3 +14,24 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+//Questão 1
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysOfMonth(){
+  let daysOfMonth = document.getElementById('days');
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let day = dezDaysList[index];
+    let days = document.createElement('li');
+    days.innerText = day;
+    days.className = 'day';
+    if(day === 24 || day === 25 || day === 31) {
+      days.className += ' holiday';  
+    }
+    if(day === 4 || day === 11 || day === 18 || day === 25) {
+      days.className += ' friday';
+    }
+    daysOfMonth.appendChild(days)
+  }
+}
+createDaysOfMonth();
