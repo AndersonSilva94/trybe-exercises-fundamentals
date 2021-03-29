d50d02fbf586b19553882876d428a42b1f39411b
Anderson Silva <andssilva.23@gmail.com>
2021-03-29 17:47:20 -0400

    aula - exercício e exemplos

---
+++ exercises/bloco_8/dia_1/aula.js
@@ -0,0 +1,52 @@
+// const repeat = (number, action) => {
+//   for (let count = 0; count <= number; count += 1) {
+//     action(count);
+//   }
+// };
+
+// repeat(5, console.table);
+
+// const repeat = (number, action) => {
+//   for (let count = 0; count <= number; count += 1) {
+//     action(count);
+//   }
+// };
+
+// repeat(3, (number) => {
+//   if (number % 2 === 0) {
+//     console.log(number, 'is even');
+//   }
+// });
+
+// const repeat = (number, action) => {
+//   for (let count = 0; count <= number; count += 1) {
+//     action(count);
+//   }
+// };
+
+// const isEven = (number) => {
+//   if (number % 2 === 0) {
+//     console.log(number, 'is even');
+//   }
+// };
+
+// const isOdd = (number) => {
+//   if ((number % 2) > 0) {
+//     console.log(number, 'is odd');
+//   }
+// };
+
+// repeat(3, isEven); // Testa quais números serão pares;
+// repeat(3, isOdd); // Testa quais números serão ímpares;
+
+const wakeUp = () => console.log("Acordando");
+
+const coffee = () => console.log("Bora tomar café!");
+
+const sleep = () => console.log("Partiu dormir!");
+
+const doThing = (action) => action();
+
+doThing(wakeUp);
+doThing(coffee);
+doThing(sleep)
