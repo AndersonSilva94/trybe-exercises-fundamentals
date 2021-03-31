46662ec8983c07b82346d870b3f0cfa14fc69b87
Anderson Silva <andssilva.23@gmail.com>
2021-03-31 12:25:54 -0400

    exercicios - parte 2 - exercício 2

--- exercises/bloco_7/dia_3/parte1-exercicio1.js
+++ exercises/bloco_7/dia_3/parte1-exercicio1.js
@@ -10,7 +10,7 @@ function sum(a, b) {
 
 // implemente seus testes aqui
 // 1 - A função sum(a, b) retorna a soma do parâmetro a com o b
-assert.strictEqual(typeof(sum), "function");
+assert.strictEqual(typeof sum, "function");
 
 // 1.1 - Teste se o retorno de sum(4, 5) é 9
 assert.strictEqual(sum(4, 5), 9, "A soma de 4 + 5 é 9");
